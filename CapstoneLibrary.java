0a2c73ddc202eee01960b72e4ce048efe22e26d6
MWessels62 <m.wessels62@gmail.com>
2019-09-12 13:42:02 +0200

    Added code to automatically launch MySQL server

--- CapstoneLibrary.java
+++ CapstoneLibrary.java
@@ -1,21 +1,46 @@
-import java.sql.*;
-//import java.util.*;
-import javax.swing.*;
 //Capstone library project
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 public class CapstoneLibrary {
-	
 	
 	Connection databaseConnection = null;
 	Statement stmt = null; 
 	ResultSet rset = null; 	
 	String executionString = "";
 
-	
-	public void dbConnection(){
+	//Establishes connection with the MySQL server using the JDBC driver
+	/*On another computer, the following (capstone_library","myuser", "xxxx") will need to be replaced by your
+		database name, user name and password*/
+	public void dbConnection() { 
+
+		//This will open up cmd.exe and initialise the MySQL server so that the user doesn't need to do this before running the program
+		try
+        { 
+            // Just one line and you are done !  
+            // We have given a command to start cmd 
+            // /K : Carries out command specified by string 
+			Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"mysqld --console\"");
+        } 
+        catch (Exception e) 
+        { 
+            System.out.println("Incorrect input"); 
+            e.printStackTrace(); 
+        } 
 		try {
-			// Step 1: Allocate a database 'Connection' object
-			//Class.forName("com.mysql.jdbc.Driver");  
+
+			// Allocate a database 'Connection' object
 			databaseConnection = DriverManager.getConnection(
 					"jdbc:mysql://localhost:3306/capstone_library","myuser", "xxxx");
 			stmt = databaseConnection.createStatement();
@@ -26,25 +51,26 @@ public class CapstoneLibrary {
 	
 	//Shows all books within the database
 	private void showAll_books()  {
-		 
+			// 'executionString' contains the mySQL instruction
 			executionString = "select * from library_db";
 			printQuery(executionString);
 	}
 
-
+	//Following code physically passes the execution instructions from the other methods to MySQL, and then prints out the result
 	private void printQuery(String commandString) {
 		try {
+			//rset object is the connection object that gets passed the instruction to be executed 
 			rset = stmt.executeQuery(commandString); 
-		
-		
-		String outputString = "";
-		int entryCount = 0;
-		while(rset.next()) {
-			entryCount += 1;
-			outputString += (entryCount + ". " + rset.getInt("id") + ", " 
-					+ rset.getString("Title") + ", " 
-					+ rset.getString("Author") + ", " 
-					+ rset.getInt("Qty") + "\n");
+				
+			String outputString = "";
+			int entryCount = 0;
+			//The output for all entries is appended onto outputString
+			while(rset.next()) {
+				entryCount += 1;
+				outputString += (entryCount + ". " + rset.getInt("id") + ", " 
+						+ rset.getString("Title") + ", " 
+						+ rset.getString("Author") + ", " 
+						+ rset.getInt("Qty") + "\n");
 			
 		}
 		JOptionPane.showMessageDialog(null,outputString,"All results from library database", JOptionPane.INFORMATION_MESSAGE);
@@ -55,11 +81,13 @@ public class CapstoneLibrary {
 			
 	private void enterBook() {
 		try {
+			//JTextFields take as input the user's input into dialog boxes
 		  JTextField newId = new JTextField(11);
 	      JTextField newTitle = new JTextField(80);
 	      JTextField newAuthor = new JTextField(50);
 	      JTextField newQty = new JTextField(11);
 
+	      //Constructing the dialogue box
 	      JPanel myPanel = new JPanel();
 	      myPanel.setLayout(new BoxLayout(myPanel, BoxLayout.Y_AXIS));
 	    
@@ -78,7 +106,7 @@ public class CapstoneLibrary {
 	      JOptionPane.showConfirmDialog(null, myPanel, 
 	               "Details of new book", JOptionPane.OK_CANCEL_OPTION);
 	      
-	      
+	      //Parse integer data from the user input
 		  String tempId = newId.getText();
 		  String tempQty = newQty.getText();
 		  int parsedId = Integer.parseInt(tempId);
@@ -108,12 +136,10 @@ public class CapstoneLibrary {
 			  } catch(SQLException ex) {
 					ex.printStackTrace();
 			  	}
-		
-		
 	}
 	
 	
-	//Worked on the assumption that the update function only needed to update the quantity of books
+	//Currently this function can only change the no. of books (i.e. it cant be used to change other features of existing books)
 	
 	private void updateBook() {
 		String inputId = JOptionPane.showInputDialog(
@@ -136,9 +162,8 @@ public class CapstoneLibrary {
 		
 	}
 	
-	
 	private void searchBooks() {
-		String searchVariable  = JOptionPane.showInputDialog(null, //I should convert this to look like the insert book function
+		String searchVariable  = JOptionPane.showInputDialog(null, 
 				"Which field would you like to search in: \n"
 				+ "1. ID\n"
 				+ "2. Title\n"
@@ -146,7 +171,7 @@ public class CapstoneLibrary {
 				+ "4. Quantity", JOptionPane.QUESTION_MESSAGE);
 		int searchVariableInt = Integer.parseInt(searchVariable);
 		
-		String searchValue  = JOptionPane.showInputDialog(null, //I should convert this to look like the insert book function
+		String searchValue  = JOptionPane.showInputDialog(null, 
 				"Type in the value that you would like to search for "
 				+ "(for quantity, the items returned will be any books that have >= the number indicated: "
 				, JOptionPane.QUESTION_MESSAGE);
@@ -168,14 +193,14 @@ public class CapstoneLibrary {
 		printQuery(executionString);
 		
 	}
-		
-		
      
 	private void printMenu() {
 		String input;
-		int input_int = -1; //Allocated -1 as arbitrary number that wasnt 0
-		
+		int input_int = -1; //Allocated -1 as arbitrary number that wasn't 0
+			
+		//printMenu function repeats after each action, until a '0' is entered
 		while (input_int != 0) { 
+			//Main menu
 			input = JOptionPane.showInputDialog(
 					null,"1. Enter book\n"
 					+ "2. Update book\n"
@@ -185,8 +210,7 @@ public class CapstoneLibrary {
 					+ "0. Exit", "Library Database: Please select an option...", JOptionPane.QUESTION_MESSAGE);
 			input_int = Integer.parseInt(input);
 			
-			switch (input_int) {
-			
+			switch (input_int) {		
 				case 1: 
 					enterBook();
 					break;
@@ -206,14 +230,10 @@ public class CapstoneLibrary {
 		}
 	}
 
-	
 	public static void main(String[] args) {
        
 		CapstoneLibrary newInstance = new CapstoneLibrary();
-       
         newInstance.dbConnection();
 		newInstance.printMenu();
-		
 	}
-
 }
